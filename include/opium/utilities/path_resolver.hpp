7b6fbfca70aa6f8c49b6fe8c27f34e9ceade8cce
Ivan Pidhurskyi <ivanpidhurskyi1997@gmail.com>
2025-06-18 14:17:35 +0200

    Add automatic include of OSL builtins.

---
+++ include/opium/utilities/path_resolver.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <filesystem>
+
+
+namespace opi {
+
+template <typename BeginIter, typename EndIter>
+std::filesystem::path
+resolve_path(const std::filesystem::path &filename, BeginIter begin,
+             EndIter end)
+{
+  for (; begin != end; ++begin)
+  {
+    const std::filesystem::path prefix = *begin;
+    const std::filesystem::path fullpath =
+        prefix == "" ? filename : prefix / filename;
+    if (std::filesystem::exists(fullpath))
+      return fullpath;
+  }
+
+  throw std::runtime_error {"Failed to resolve path"};
+}
+
+} // namespace opi
